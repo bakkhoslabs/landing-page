ccc8b6974998080f14a171c30c31cd04b19cae36
Zhang Zeyu <zhangzeyu2001@gmail.com>
2023-09-24 21:42:17 +0000

    fix: typing errors

--- components/util/headings.tsx
+++ components/util/headings.tsx
@@ -1,4 +1,9 @@
-const GoldHeading1 = ({ text, fadeUp }) => {
+type HeadingProps = {
+  text: string;
+  fadeUp?: boolean;
+};
+
+const GoldHeading1 = ({ text, fadeUp }: HeadingProps) => {
   return (
     <h1 className="h1 mb-4 bg-gradient-to-r from-amber-300 to-yellow-500 inline-block text-transparent bg-clip-text" data-aos={fadeUp ? "fade-up" : ""}>
       {text}
@@ -6,7 +11,7 @@ const GoldHeading1 = ({ text, fadeUp }) => {
   )
 }
 
-const SlateHeading2 = ({ text, fadeUp }) => {
+const SlateHeading2 = ({ text, fadeUp }: HeadingProps) => {
   return (
     <h2 className="h2 mb-4 bg-gradient-to-r from-slate-300 to-slate-400 inline-block text-transparent bg-clip-text" data-aos={fadeUp ? "fade-up" : ""}>
       {text}
@@ -14,7 +19,7 @@ const SlateHeading2 = ({ text, fadeUp }) => {
   )
 }
 
-const GoldHeading2 = ({ text, fadeUp }) => {
+const GoldHeading2 = ({ text, fadeUp }: HeadingProps) => {
   return (
     <h2 className="h2 mb-4 bg-gradient-to-r from-amber-300 to-yellow-500 inline-block text-transparent bg-clip-text" data-aos={fadeUp ? "fade-up" : ""}>
       {text}
@@ -22,7 +27,7 @@ const GoldHeading2 = ({ text, fadeUp }) => {
   )
 }
 
-const SlateHeading4 = ({ text, fadeUp }) => {
+const SlateHeading4 = ({ text, fadeUp }: HeadingProps) => {
   return (
     <h4 className="h4 mb-4 bg-gradient-to-r from-slate-300 to-slate-400 inline-block text-transparent bg-clip-text" data-aos={fadeUp ? "fade-up" : ""}>
       {text}
